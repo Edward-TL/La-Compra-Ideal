205799d4618fa704d6380afbe43cead58e7d3e70
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-20 18:31:42 -0500

    adapt_url test added. Passed it and the change in the test of there_is_soup to this function

--- web scraper/tests.py
+++ web scraper/tests.py
@@ -1,45 +1,53 @@
 import unittest
 from scrape_funcs import extract_soup
-from scrape_data import replacers, urls, headers
+from scrape_data import Amazon
 
-class TestScraper(unittest.TestCase):
+class Test_Amazon_Properties(unittest.TestCase):
     #Replacers
     def test_user_request_amazon_adaption(self):
         user_request = 'audifonos inalambricos'
-        amz_user_request_edited = user_request.replace(' ', replacers.amazon)
+        amz_user_request_edited = user_request.replace(' ', Amazon.space_replacer)
 
         self.assertNotEqual(user_request, amz_user_request_edited)
-        
-
-    def test_user_request_wallmart_adaption(self):
-        user_request = 'audifonos inalambricos'
-        wm_user_request_edited = user_request.replace(' ', replacers.wallmart)
-
-        self.assertNotEqual(user_request, wm_user_request_edited)
-    
-    def test_user_request_ml_adaption(self):
-        user_request = 'audifonos inalambricos'
-        ml_user_request_edited = user_request.replace(' ', replacers.mercado_libre)
-
-        self.assertNotEqual(user_request, ml_user_request_edited)
 
     def test_conection_status(self):
         user_request = 'audifonos inalambricos'
-        amz_user_request_edited = user_request.replace(' ', replacers.amazon)
-        amz_url = urls.amazon.replace('{}', amz_user_request_edited)
+        amz_user_request_edited = user_request.replace(' ', Amazon.space_replacer)
+        amz_url = Amazon.url.replace(Amazon.url_replacers[0], '.mx')
+        amz_url = amz_url.replace(Amazon.url_replacers[1], amz_user_request_edited)
+
         amz_status = extract_soup(amz_url, 0, just_status=True)
 
         self.assertEqual(amz_status,200)
 
-    def test_theres_soup(self):
+    def test_adapt_url(self):
         user_request = 'audifonos inalambricos'
-        amz_user_request_edited = user_request.replace(' ', replacers.amazon)
-        amz_url = urls.amazon.replace('{}', amz_user_request_edited)
-        amz_soup = extract_soup(amz_url, 0, just_soup=True)
+        country = 'mx'
+        amz_url = Amazon.adapt_url(Amazon, country, user_request)
+
+        self.assertEqual(amz_url, 'https://www.amazon.com.mx/s?k=audifonos+inalambricos')
+
+
+    def test_there_is_soup(self):
+        user_request = 'audifonos inalambricos'
+        country = 'mx'
+        amz_url = Amazon.adapt_url(Amazon, country, user_request)
+
+        amz_soup = extract_soup(amz_url, 1, just_soup=True)
 
         self.assertIsNotNone(amz_soup)
 
+    # def test_user_request_wallmart_adaption(self):
+    #     user_request = 'audifonos inalambricos'
+    #     wm_user_request_edited = user_request.replace(' ', replacers.wallmart)
+
+    #     self.assertNotEqual(user_request, wm_user_request_edited)
     
+    # def test_user_request_ml_adaption(self):
+    #     user_request = 'audifonos inalambricos'
+    #     ml_user_request_edited = user_request.replace(' ', replacers.mercado_libre)
+
+    #     self.assertNotEqual(user_request, ml_user_request_edited)
     
 
 
