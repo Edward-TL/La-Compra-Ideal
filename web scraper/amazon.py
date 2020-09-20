87e6bdb5b2784b767d78c8baae9c7b9b6792d6f5
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-20 18:33:30 -0500

    change get_this_info to get_info, and added the adapt_url function

--- web scraper/amazon.py
+++ web scraper/amazon.py
@@ -1,17 +1,15 @@
 from scrape_data import Amazon
-from scrape_funcs import extract_soup, search_boxes, get_this_info
+from scrape_funcs import extract_soup, search_boxes, get_info
 from bs4 import BeautifulSoup
 
 user_request = 'audifonos inalambricos'
-amz_user_request = user_request.replace(' ', Amazon.space_replacer)
-amz_url = Amazon.url.replace(Amazon.url_replacers[1], amz_user_request)
-
-amz_url = amz_url.replace('{country}', '.mx')
+country = 'mx'
+amz_url = Amazon.adapt_url(Amazon, country, user_request)
 amz_soup = extract_soup(amz_url, 1, just_soup=True)
 
 amz_boxes = search_boxes(amz_soup, Amazon.boxes)
 
-amazon_info = get_this_info(amz_boxes, Amazon.stars)
+amazon_info = get_info(amz_boxes, Amazon.stars)
 print(amazon_info)
 # for highlight in amazon_highlightners:
 #     print(highlight)
