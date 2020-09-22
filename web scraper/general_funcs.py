9ff9e329430e21269170e2e713b44560287a40a3
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-21 21:15:38 -0500

    cheapest and get_cheapest functiones build. Get cheapest returns a dictionary object that has all the relevant information that the user considers

--- web scraper/general_funcs.py
+++ web scraper/general_funcs.py
@@ -3,3 +3,34 @@ def ordered_dict(dictionary, reverse=True):
     ordered_dict = dict(sorted(dictionary.items(), key=operator.itemgetter(1),reverse=True))
 
     return ordered_dict
+
+def cheapest(array_prices, just_position=True, just_price=False, test=False):
+    cheapest_position = 0
+    cheapest_price = array_prices[0]
+
+    for n in range(len(array_prices)):
+        price = array_prices[n]
+        if test == True:
+            print(f'number = {n} cheapest_price is {cheapest_price} and price check is {price}')
+
+        if price < cheapest_price:
+            if test == True:
+                print(f'price: {price} < {cheapest_price}')
+
+            cheapest_price = price
+            cheapest_position = n
+        
+    if just_position == True:
+        return cheapest_position
+    elif just_price == True:
+        return cheapest_price
+    else:
+        return cheapest_position, cheapest_price
+
+def get_cheapest(cheapest, products_dictionary):
+    cheapest_dict = {}
+
+    for key in products_dictionary:
+        cheapest_dict[key] = products_dictionary[key][cheapest]
+    
+    return cheapest_dict
