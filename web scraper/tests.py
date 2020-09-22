119758c6db49747548c9f94237268b9a3237686b
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-21 21:44:53 -0500

    Because of the failure in adapt_url's function of Mercado_Libre, the decission of making folders for each page is consider. Decission pending.

--- web scraper/tests.py
+++ web scraper/tests.py
@@ -1,8 +1,9 @@
 import unittest
 
-from scrape_data import Amazon
+from scrape_data import Amazon, Mercado_Libre
 from scrape_funcs import extract_soup, search_boxes, get_brute_info
 from data_filters import get_names, get_images, get_products_urls, get_price
+
 #Amazon Only
 from data_filters import get_stars, get_reviews, amazon_products_id
 
@@ -94,18 +95,45 @@ class Test_Amazon_Properties_And_Functions(unittest.TestCase):
         for test in trials:
             self.assertEquals(len(amazon_boxes), test)
 
+class Test_Mercado_Libre_Properties_And_Functions(unittest.TestCase):
+    def test_user_request_ml_adaption(self):
+        user_request = 'audifonos inalambricos'
+        ml_user_request_edited = user_request.replace(' ', Mercado_Libre.space_replacer)
 
-    # def test_user_request_wallmart_adaption(self):
-    #     user_request = 'audifonos inalambricos'
-    #     wm_user_request_edited = user_request.replace(' ', replacers.wallmart)
+        self.assertNotEqual(user_request, ml_user_request_edited)
 
-    #     self.assertNotEqual(user_request, wm_user_request_edited)
+    def test_mercado_libre_conection_status(self):
+        user_request = 'audifonos inalambricos'
+        ml_user_request_edited = user_request.replace(' ', Mercado_Libre.space_replacer)
+        ml_url = Mercado_Libre.url.replace(Mercado_Libre.url_replacers[0], '.mx')
+        ml_url = ml_url.replace(Mercado_Libre.url_replacers[1], ml_user_request_edited)
+
+        ml_status = extract_soup(ml_url, 0, just_status=True)
+
+        self.assertEqual(ml_status,200)
+
+    def test_adapt_url(self):
+        user_request = 'audifonos inalambricos'
+        country = 'mx'
+        ml_url = Mercado_Libre.adapt_url(Mercado_Libre, country, user_request)
+
+        self.assertEqual(ml_url, 'https://listado.mercadolibre.com.mx/audifonos-inalambricos#D[A:audifonos%20inalambricos]')
+
+
+    def test_there_is_soup(self):
+        user_request = 'audifonos inalambricos'
+        country = 'mx'
+        ml_url = Mercado_Libre.adapt_url(Mercado_Libre, country, user_request)
+
+        ml_soup = extract_soup(ml_url, 1, just_soup=True)
+
+        self.assertIsNotNone(ml_soup)
+
+# class Test_ebay_Properties_And_Functions(unittest.TestCase):
+
+# class Test_wallmart_Properties_And_Functions(unittest.TestCase):
     
-    # def test_user_request_ml_adaption(self):
-    #     user_request = 'audifonos inalambricos'
-    #     ml_user_request_edited = user_request.replace(' ', replacers.mercado_libre)
 
-    #     self.assertNotEqual(user_request, ml_user_request_edited)
     
 
 
