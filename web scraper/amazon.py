ca2b5454765ce42193c46b9053f085ba9c24e39b
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-21 21:14:01 -0500

    cheapest product obtained

--- web scraper/amazon.py
+++ web scraper/amazon.py
@@ -1,6 +1,8 @@
-from scrape_data import Amazon
+from scrape_data import Amazon, Products
 from scrape_funcs import extract_soup, search_boxes, get_brute_info
 from data_filters import get_names, get_images, get_products_urls, get_price
+
+from general_funcs import cheapest, get_cheapest
 
 from data_filters import get_stars, get_reviews, amazon_products_id
 
@@ -15,34 +17,31 @@ amazon_soup = extract_soup(amazon_url, 1, just_soup=True)
 
 #HTML divided by products, and stored as elements of an array
 amazon_boxes = search_boxes(amazon_soup, Amazon.boxes)
+amazon_products = {}
 
-# amazon_names = get_names(amazon_boxes, Amazon.name_and_images)
-# # print(amazon_names)
+amazon_products['names'] = get_names(amazon_boxes, Amazon.name_and_images)
 
-# '''Amazon's images source (link)'''
-# amazon_images = get_images(amazon_boxes, Amazon.name_and_images)
-# # print(amazon_images)
+'''Amazon's images source (link)'''
+amazon_products['images'] = get_images(amazon_boxes, Amazon.name_and_images)
 
-# amazon_urls = get_products_urls(amazon_boxes, Amazon.product_urls)
-# # print(len(amazon_urls))
+amazon_products['urls'] = get_products_urls(amazon_boxes, Amazon.product_urls)
 
-# '''Just Amazon's products id. Is used as a url generator:
-# amazon's url + domain + "/dp/" + product_id'''
-# amazon_ids= amazon_products_id(amazon_boxes)
-# # print(len(amazon_ids))
+'''Just Amazon's products id. Is used as a url generator:
+amazon's url + domain + "/dp/" + product_id'''
+amazon_products['ids']= amazon_products_id(amazon_boxes)
 
-# '''Just stars as float'''
-# amazon_stars = get_stars(country, amazon_boxes, Amazon.stars)
-# # # print(amazon_stars)
-# '''Just number of reviews as int'''
-# amazon_reviews = get_reviews(country, amazon_boxes, Amazon.reviews)
-# # # print(amazon_reviews)
+'''Just stars as float'''
+amazon_products['stars'] = get_stars(country, amazon_boxes, Amazon.stars)
 
-amazon_reviews = get_price(country, amazon_boxes, Amazon.price)
-print(len(amazon_reviews))
-print(amazon_reviews)
+'''Just number of reviews as int'''
+amazon_products['reviews'] = get_reviews(country, amazon_boxes, Amazon.reviews)
 
+amazon_products['prices'] = get_price(country, amazon_boxes, Amazon.price)
+# print(len(amazon_reviews))
 
-
+cheapest = cheapest(amazon_products['prices'])
+cheapest_amazon_product = get_cheapest(cheapest, amazon_products)
+for key in cheapest_amazon_product:
+    print(key, ':', cheapest_amazon_product[key])
 # for highlight in amazon_highlightners:
 #     print(highlight)
