04d955d5b7db61fe76dd3d7f65fc1acec8b8e3b6
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-21 21:18:13 -0500

    test printers added, also solved the bug of urls added 'https://' string

--- web scraper/data_filters.py
+++ web scraper/data_filters.py
@@ -4,7 +4,7 @@ import requests
 from scrape_funcs import search_boxes
 from scrape_data import coins_dict
 
-def get_names(boxes_array, info_tuple):
+def get_names(boxes_array, info_tuple, test=False):
     names = [None]*len(boxes_array)
 
     b=0
@@ -15,22 +15,26 @@ def get_names(boxes_array, info_tuple):
             names[b] = searcher[0].img.get('alt')
             
         b +=1
+    if test == True:
+        print(names)
     return names
 
-def get_images(boxes_array, info_tuple):
-    names = [None]*len(boxes_array)
+def get_images(boxes_array, info_tuple, test=False):
+    images = [None]*len(boxes_array)
 
     b=0
     for box in boxes_array:
         #Remember that boxes are arrays
         searcher = search_boxes(box, info_tuple)
         if searcher:
-            names[b] = searcher[0].img.get('src')
+            images[b] = searcher[0].img.get('src')
             
         b +=1
-    return names
+    if test == True:
+        print(images)
+    return images
 
-def get_price(country, boxes_array, info_tuple):
+def get_price(country, boxes_array, info_tuple, test=False):
     price = [None]*len(boxes_array)
 
     coin_symbol = coins_dict[country]
@@ -54,10 +58,11 @@ def get_price(country, boxes_array, info_tuple):
                 
                 
         b +=1
-    
+    if test == True:
+        print(price)
     return price
 
-def get_products_urls(boxes_array, info_tuple):
+def get_products_urls(boxes_array, info_tuple, test=False):
     urls = [None]*len(boxes_array)
 
     b=0
@@ -66,12 +71,14 @@ def get_products_urls(boxes_array, info_tuple):
         searcher = search_boxes(box, info_tuple)
         if searcher:
             source_url = searcher[0].get('href')
-            urls[b] = 'www.amazon.com.mx' + source_url
+            urls[b] = 'https://www.amazon.com.mx' + source_url
             
         b +=1
+    if test == True:
+        print(urls)
     return urls
 
-def get_stars(country, boxes_array, info_tuple):
+def get_stars(country, boxes_array, info_tuple, test=False):
     stars = [None]*len(boxes_array)
 
     b=0
@@ -86,23 +93,11 @@ def get_stars(country, boxes_array, info_tuple):
                 stars[b] = float(searcher[0].get_text()[:3].replace(',','.'))
 
         b +=1
-    
+    if test == True:
+        print(stars)
     return stars
 
-def amazon_products_id(boxes_array):
-    ids = [None]*len(boxes_array)
-
-    b=0
-    for box in boxes_array:
-        #Remember that boxes are arrays
-        if box:
-            product_id = box.get('data-asin')
-            ids[b] = 'www.amazon.com.mx/dp/' + product_id
-            
-        b +=1
-    return ids
-
-def get_reviews(country, boxes_array, info_tuple):
+def get_reviews(country, boxes_array, info_tuple, test=False):
     reviews = [None]*len(boxes_array)
 
     b=0
@@ -126,5 +121,22 @@ def get_reviews(country, boxes_array, info_tuple):
                     pass
         
         b +=1
+    if test == True:
+        print(reviews)
     return reviews
+
+def amazon_products_id(boxes_array, test=False):
+    ids = [None]*len(boxes_array)
+
+    b=0
+    for box in boxes_array:
+        #Remember that boxes are arrays
+        if box:
+            product_id = box.get('data-asin')
+            ids[b] = 'www.amazon.com.mx/dp/' + product_id
+            
+        b +=1
+    if test == True:
+        print(ids)
+    return ids
 
